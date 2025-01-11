e9054248b5bdf5e3323f0b4c61455c25d14b9808
void <wordhh1@gmail.com>
2025-01-11 13:38:03 +0000

    Merge NoFortuneChestClient.java and NoFortuneChest.java to only have client entry point

--- src/main/java/com/thevoidblock/nofortunechest/NoFortuneChest.java
+++ src/main/java/com/thevoidblock/nofortunechest/NoFortuneChest.java
@@ -2,19 +2,20 @@ package com.thevoidblock.nofortunechest;
 
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class NoFortuneChest implements ModInitializer {
+public class NoFortuneChest implements ClientModInitializer {
 
     public static final String MOD_ID = "nofortunechest";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
-    public void onInitialize() {
+    public void onInitializeClient() {
 
         AutoConfig.register(ModConfig.class, JanksonConfigSerializer::new);
+        NonSilkWarning.initializeDetection();
 
         LOGGER.info(String.format("%s initialized!", MOD_ID));
     }
